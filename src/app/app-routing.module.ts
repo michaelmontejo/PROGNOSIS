5080053e39fcd7a72855d7bb777ce70bb7585c09
michaelmontejo <108599053+michaelmontejo@users.noreply.github.com>
2024-04-24 19:17:10 -0500

    routes imagenes3d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { Imagenes2dComponent } from "./imagenes2d/imagenes2d.component";
+import { Imagenes3dComponent } from "./imagenes3d/imagenes3d.component";
 
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   {path: 'imagenes2d', component: Imagenes2dComponent},
+  {path: 'imagenes3d', component: Imagenes3dComponent}
 ];
 
 @NgModule({
